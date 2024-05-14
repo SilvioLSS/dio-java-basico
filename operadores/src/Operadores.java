b84095d439af6462b43368a11045467fea7f4128
silvinls <silvioluizs.santos@gmail.com>
2024-05-14 11:07:18 -0300

    operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+    int idade = 17;
+
+    if (idade >= 18)
+        System.out.println("Você é maior de idade");
+    else
+        System.out.println("Você é menor de idade!");
+    
+}
+}
