79bba8f35011a021a67f2262db3fca62d6f29d5b
silvinls <silvioluizs.santos@gmail.com>
2024-06-17 09:04:37 -0300

    Pesquisa com SET

--- Set/Operações Básicas SET/src/Convidado.java
+++ Set/Operações Básicas SET/src/Convidado.java
@@ -18,6 +18,30 @@ public class Convidado {
         return codigoConvite;
     }
 
+    
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigoConvite;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Convidado other = (Convidado) obj;
+        if (codigoConvite != other.codigoConvite)
+            return false;
+        return true;
+    }
+
     @Override
     public String toString() {
         return "Convidado [nome=" + nome + ", codigoConvite=" + codigoConvite + "]";
