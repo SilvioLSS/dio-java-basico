dad8b716a42abfa3c87eaff67e68d143b36470ed
silvinls <silvioluizs.santos@gmail.com>
2024-06-17 09:37:59 -0300

    Ordenação SET

---
+++ Ordenação SET/src/ComparatorPorPreco.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+class ComparatorPorPreco implements Comparator<Produto> {
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
+
+}
---
+++ Ordenação SET/src/Produto.java
@@ -0,0 +1,62 @@
+public class Produto implements Comparable<Produto>{
+    // atributos
+    private String nome;
+    private long codigo;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, long codigo, double preco, int quantidade) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (codigo ^ (codigo >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (codigo != other.codigo)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto [nome=" + nome + ", codigo=" + codigo + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+    }
+}
