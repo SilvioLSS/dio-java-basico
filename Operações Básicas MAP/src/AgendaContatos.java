afe5cbaa7533f4aa8926db862abec874ae164b57
silvinls <silvioluizs.santos@gmail.com>
2024-06-18 12:59:13 -0300

    Pesquisa MAP

---
+++ Operações Básicas MAP/src/AgendaContatos.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    // Atributos
+    private Map<String, Integer> agendaContatosMap;
+
+    public AgendaContatos() {
+        this.agendaContatosMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone) {
+        this.agendaContatosMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if (!this.agendaContatosMap.isEmpty()) {
+            this.agendaContatosMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos() {
+        System.out.println(this.agendaContatosMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        return this.agendaContatosMap.get(nome);
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        // Adicionar contatos
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("João", 5665);
+        agendaContatos.adicionarContato("Carlos", 1111111);
+        agendaContatos.adicionarContato("Ana", 654987);
+        agendaContatos.adicionarContato("Maria", 1111111);
+        agendaContatos.adicionarContato("Camila", 44444);
+
+        agendaContatos.exibirContatos();
+
+        // Remover um contato
+        agendaContatos.removerContato("Maria");
+        agendaContatos.exibirContatos();
+
+        // Pesquisar número por nome
+        String nomePesquisa = "João";
+        Integer numeroPesquisa = agendaContatos.pesquisarPorNome(nomePesquisa);
+        System.out.println("Número de telefone de " + nomePesquisa + ": " + numeroPesquisa);
+
+        String nomePesquisaNaoExistente = "Paula";
+        Integer numeroPesquisaNaoExistente = agendaContatos.pesquisarPorNome(nomePesquisaNaoExistente);
+        System.out.println("Número de telefone de " + nomePesquisaNaoExistente + ": " + numeroPesquisaNaoExistente);
+    }
+}
